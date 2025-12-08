18fca5d74c77d9a70462587acc94dfb0cda64052
Leonhard Kipp <Leonhard.Kipp@alumni.fh-aachen.de>
2025-12-08 19:35:11 +0100

    d7 part 2

--- d7/main.py
+++ d7/main.py
@@ -18,24 +18,33 @@ def read_grid():
 
 def progress_beams(grid: list[str]):
     beams = set()
+    beam_counts = [0 for i in range(len(grid[0]))]
+
     beams.add(grid[0].find('S'))
+    beam_counts[grid[0].find('S')] += 1
 
     splitted_total = 0
     for row_idx in range(1, len(grid)):
         new_beams = set()
+        new_beam_counts = [0 for i in range(len(grid[0]))]
+
         for beam in beams:
             next_spot = grid[row_idx][beam]
             if next_spot == '.':
                 new_beams.add(beam)
+                new_beam_counts[beam] += beam_counts[beam]
             elif next_spot == '^':
                 splitted_total += 1
                 if beam > 0:
                     new_beams.add(beam - 1)
+                    new_beam_counts[beam - 1] += beam_counts[beam]
                 if beam < len(grid[0]):
                     new_beams.add(beam + 1)
-        print("row_idx", row_idx, "splitted_total", splitted_total)
+                    new_beam_counts[beam + 1] += beam_counts[beam]
         beams = new_beams
-    print(splitted_total)
+        beam_counts = new_beam_counts
+        print("row_idx %2d, beam_counts %2d" % (row_idx, sum(beam_counts)), "beams", beam_counts)
+    print(sum(beam_counts))
 
 
 if __name__ == "__main__":
